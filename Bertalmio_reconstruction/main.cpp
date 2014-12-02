557e6e985c4f705e0bddd3cea50b970daa4cba87
tucna <pavel.vlasanek@tucna.net>
2014-12-02 11:54:18 +0100

    algorithm estabilish

--- Bertalmio_reconstruction/main.cpp
+++ Bertalmio_reconstruction/main.cpp
@@ -4,6 +4,9 @@
 
 #include "bertalmioprocessing.h"
 
+const int A = 15;
+const int B = 2;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -19,6 +22,7 @@ int main(int argc, char *argv[])
     else
     {
         QImage anisotropic;
+        bool stable = false;
 
         BertalmioProcessing::List2DFloat gradientInput;
         BertalmioProcessing::List2DFloat partialResult;
@@ -27,14 +31,36 @@ int main(int argc, char *argv[])
         BertalmioProcessing::List2DInt beta;
         BertalmioProcessing::List2DInt laplace;
 
+        // Whole imahe should goes thru anisotropic diffusion
+        anisotropic = bertalmioParts.anisotropicDiffusion(input);
+
+        // ...after that go to the loop
+        while (!stable)
+        {
+            for(int i = 0; i < A; i++)
+            {
+                //...few iterations of inpainting
+                laplace = bertalmioParts.laplace_7(input);
+                isophoteDirection = bertalmioParts.isophoteDirection_8(input);
+                beta = bertalmioParts.beta_9(gradientLaplace, isophoteDirection);
+                gradientInput = bertalmioParts.gradientInput_10(input, beta);
+            }
+
+            for(int i = 0; i < B; i++)
+            {
+                // ...few iterations of anisotropic diffusion.
+                anisotropic = bertalmioParts.anisotropicDiffusion(input);
+            }
+        }
+
+        /*
         laplace = bertalmioParts.laplace_7(input);
         gradientLaplace = bertalmioParts.gradientLaplace_6(laplace);
         isophoteDirection = bertalmioParts.isophoteDirection_8(input);
         beta = bertalmioParts.beta_9(gradientLaplace, isophoteDirection);
         gradientInput = bertalmioParts.gradientInput_10(input, beta);
         partialResult = bertalmioParts.partialResult_5(beta, gradientInput);
-
-        anisotropic = bertalmioParts.anisotropicDiffusion(input);
+        */
 
         //qDebug() << gradientLaplace.r[9][10].x << gradientLaplace.r[9][10].y;
         //qDebug() << isophoteDirection.r[4][4].x << isophoteDirection.r[4][4].y;
